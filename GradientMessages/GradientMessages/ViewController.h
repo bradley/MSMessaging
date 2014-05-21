d21cb8378bfe0028a8d43db0ad51d31deeaf6b42
Mark Stultz <mstultz@gmail.com>
2014-05-21 00:13:34 -0500

    Adding initial Xcode project.

---
+++ GradientMessages/GradientMessages/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GradientMessages
+//
+//  Created by Mark Stultz on 5/21/14.
+//  Copyright (c) 2014 Mark Stultz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GradientMessages/GradientMessagesTests/GradientMessagesTests.m
@@ -0,0 +1,34 @@
+//
+//  GradientMessagesTests.m
+//  GradientMessagesTests
+//
+//  Created by Mark Stultz on 5/21/14.
+//  Copyright (c) 2014 Mark Stultz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GradientMessagesTests : XCTestCase
+
+@end
+
+@implementation GradientMessagesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
