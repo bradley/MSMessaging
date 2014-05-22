580d08bd3f4450dd039a6805a6a757f8f2c0c9a7
Mark Stultz <mstultz@gmail.com>
2014-05-21 20:42:24 -0500

    Seed random

--- GradientMessages/GradientMessages/AppDelegate.m
+++ GradientMessages/GradientMessages/AppDelegate.m
@@ -12,8 +12,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
-    return YES;
+	srand(time(nil));
+	return YES;
 }
 							
 - (void)applicationWillResignActive:(UIApplication *)application
