e182b22cb87e685342c8262f8a1f7d945395a06a
Mark Stultz <mstultz@gmail.com>
2014-05-27 20:37:02 -0500

    Adding MSMessaging.xcodeproj

---
+++ MSMessaging/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MSMessaging
+//
+//  Created by Mark Stultz on 5/27/14.
+//  Copyright (c) 2014 Mark Stultz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
